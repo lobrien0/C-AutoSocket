b0004825dbde2c239ad68f15ca263bcf1255124b
skymotic <lobr266238@gmail.com>
2023-04-24 20:34:14 +0000

    Added loop to send many messages

--- Client.c
+++ Client.c
@@ -9,6 +9,7 @@
 void getMessage(char* buffer, size_t maxStr)
 {
     bzero(buffer, maxStr);
+    printf("Please Enter The Message You'd Like to Send below:\t(To Exit, type: EXIT)\n\t");
     fgets(buffer, maxStr, stdin);
 }
 
@@ -20,24 +21,30 @@ int main()
     char ip[] = "127.0.0.1";
 
     struct AutoSocket sock;
-
     char* buffer = (char*)malloc(MAXSTR * sizeof(char));
-    printf("Please Enter The Message You'd Like to Send:\n\t");
-    getMessage(buffer, MAXSTR);
 
-    createSocket(&sock, ip, port, proto);
-    sock.lineBufferLen = MAXSTR;
-    printf("[+] Socket Created!\n");
+    while(strncmp(buffer, "EXIT", 4) != 0)
+    {
+        getMessage(buffer, MAXSTR);
 
-    connectToSocket(&sock);
-    printf("[+] Socket Connected\n");
+        if(strncmp(buffer, "EXIT", 4) != 0)
+        {
+            createSocket(&sock, ip, port, proto);
+            sock.lineBufferLen = MAXSTR;
+            printf("[+] Socket Created!\n");
 
-    sendToSocket(&sock, buffer);
-    printf("[+] Message Sent!\n");
+            connectToSocket(&sock);
+            printf("[+] Socket Connected\n");
 
-    receiveFromSocket(&sock, buffer);
-    printf("Message:\n----- ----- -----\n%s\n----- ----- -----\n", buffer);
-    
-    closeSocket(&sock);
-    printf("[+] Socket closed!\n");
+            sendToSocket(&sock, buffer);
+            printf("[+] Message Sent!\n");
+
+            receiveFromSocket(&sock, buffer);
+            printf("Message:\n----- ----- -----\n%s\n----- ----- -----\n", buffer);
+            
+            closeSocket(&sock);
+            printf("[+] Socket closed!\n");
+        }
+    }
+    return 0;
 }
